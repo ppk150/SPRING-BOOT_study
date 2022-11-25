3ca4d5195c7dc203b0b8c0b87ebddaa6ce0fbb6b
S2-CHICKEN <67466148+ppk150@users.noreply.github.com>
2022-11-25 10:23:01 +0900

    Add files via upload

---
+++ section4-week1/src/test/java/com/codestates/section4week1/Section4Week1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.section4week1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section4Week1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
