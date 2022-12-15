6917e7b928aa81e39bfc4d532beb10064a10bd86
S2-CHICKEN <67466148+ppk150@users.noreply.github.com>
2022-12-15 15:03:00 +0900

    Add files via upload

---
+++ be-template-api-documentation/src/test/java/com/codestates/Section3Week3TemplateApiDocumentationApplicationTests.java
@@ -0,0 +1,15 @@
+package com.codestates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootTest
+class Section3Week3TemplateApiDocumentationApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
